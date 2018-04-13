3ce39cff37abfb57719c6deda0c22380bc6460e1
Naufal <naufalrzld@gmail.com>
2018-04-13 21:13:34 +0700

    Init Commit

---
+++ app/src/main/java/naufal/com/motionsg/MainActivity.java
@@ -0,0 +1,38 @@
+package naufal.com.motionsg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (username.equals("naufal") && password.equals("123")) {
+                    Toast.makeText(getApplicationContext(), "Benar", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Username atau password salah", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
